da48bd6700f7e68df44986470992001611c68d94
wai3aa1sam <wai3aa1sam5269@gmail.com>
2023-05-31 18:21:57 +0800

    nmsp_core - add all modules

--- src/core/src/nmsp_core/common/nmsp_core_common.h
+++ src/core/src/nmsp_core/common/nmsp_core_common.h
@@ -2,9 +2,12 @@
 
 #include <nmsp_core_base.h>
 #include <nmsp_stl.h>
+#include <nmsp_os.h>
 #include <nmsp_log.h>
 #include <nmsp_profiler.h>
 #include <nmsp_math.h>
+#include <nmsp_file_io.h>
+#include <nmsp_job_system.h>
 
 #include "nmsp_core-config.h"
 #include "nmsp_core_traits.h"
