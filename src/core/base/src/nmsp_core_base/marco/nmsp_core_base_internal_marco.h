b946d39adecc92eb29accf806ddb26851bcce0f4
wai3aa1sam <wai3aa1sam5269@gmail.com>
2025-04-27 23:39:03 +0800

    update: update profiler marco

--- src/core/base/src/nmsp_core_base/marco/nmsp_core_base_internal_marco.h
+++ src/core/base/src/nmsp_core_base/marco/nmsp_core_base_internal_marco.h
@@ -10,42 +10,46 @@
 	#include <tracy/TracyC.h>
 	#include <common/TracySystem.hpp>
 
-	#define _NMSP_PROFILE_CALLSTACK_DEPTH			32
+	#define _NMSP_PROFILE_CALLSTACK_DEPTH							32
 
-	#define _NMSP_PROFILE_FRAME()					FrameMark			
+	#define _NMSP_PROFILE_FRAME()									FrameMark			
 
-	#define _NMSP_PROFILE_SCOPED()					ZoneScopedS(NMSP_PROFILE_CALLSTACK_DEPTH)			
-	#define _NMSP_PROFILE_SECTION(NAME)				ZoneScopedNS(NAME, NMSP_PROFILE_CALLSTACK_DEPTH)
-	#define _NMSP_PROFILE_DYNAMIC_SCOPED()			ZoneTransientS( ___tracy_scoped_zone, NMSP_PROFILE_CALLSTACK_DEPTH, true)			
-	#define _NMSP_PROFILE_DYNAMIC_SECTION(NAME)		ZoneTransientNS(___tracy_scoped_zone, NAME, NMSP_PROFILE_CALLSTACK_DEPTH, true)
+	#define _NMSP_PROFILE_SECTION_IMPL(MSrcLocData, ...)			tracy::ScopedZone NMSP_VAR_NAME(_scoped)((MSrcLocData), NMSP_PROFILE_CALLSTACK_DEPTH, true)
 
-	#define _NMSP_PROFILE_TAG(STR)					ZoneText(STR, ::strlen(STR))
-	#define _NMSP_PROFILE_LOG(TEXT)					TracyMessage(TEXT, ::strlen(TEXT))
-	#define _NMSP_PROFILE_VALUE(TEXT, VALUE)		TracyPlot(TEXT, VALUE)	
+	#define _NMSP_PROFILE_SCOPED()									ZoneScopedS(NMSP_PROFILE_CALLSTACK_DEPTH)			
+	#define _NMSP_PROFILE_SECTION(NAME)								ZoneScopedNS(NAME, NMSP_PROFILE_CALLSTACK_DEPTH)
+	#define _NMSP_PROFILE_DYNAMIC_SCOPED()							ZoneTransientS( ___tracy_scoped_zone, NMSP_PROFILE_CALLSTACK_DEPTH, true)			
+	#define _NMSP_PROFILE_DYNAMIC_SECTION(NAME)						ZoneTransientNS(___tracy_scoped_zone, NAME, NMSP_PROFILE_CALLSTACK_DEPTH, true)
 
-	#define _NMSP_PROFILE_ALLOC(PTR, SIZE)			TracyCAllocS(PTR, SIZE, NMSP_PROFILE_CALLSTACK_DEPTH)    
-	#define _NMSP_PROFILE_FREE(PTR, SIZE)			TracyCFreeS(PTR, NMSP_PROFILE_CALLSTACK_DEPTH)		    
+	#define _NMSP_PROFILE_TAG(STR)									ZoneText(STR, ::strlen(STR))
+	#define _NMSP_PROFILE_LOG(TEXT)									TracyMessage(TEXT, ::strlen(TEXT))
+	#define _NMSP_PROFILE_VALUE(TEXT, VALUE)						TracyPlot(TEXT, VALUE)	
 
-	#define _NMSP_PROFILE_SET_THREAD_NAME(NAME)		::tracy::SetThreadName(NAME)
+	#define _NMSP_PROFILE_ALLOC(PTR, SIZE)							TracyCAllocS(PTR, SIZE, NMSP_PROFILE_CALLSTACK_DEPTH)    
+	#define _NMSP_PROFILE_FREE(PTR, SIZE)							TracyCFreeS(PTR, NMSP_PROFILE_CALLSTACK_DEPTH)		    
+
+	#define _NMSP_PROFILE_SET_THREAD_NAME(NAME)						::tracy::SetThreadName(NAME)
 
 #else
 
 	#define _NMSP_PROFILE_CALLSTACK_DEPTH			0
 	
-	#define _NMSP_PROFILE_FRAME()					do{ ; } while(false)	
+	#define _NMSP_PROFILE_FRAME()									do{ ; } while(false)	
 	
-	#define _NMSP_PROFILE_SCOPED()					do{ ; } while(false)
-	#define _NMSP_PROFILE_SECTION(NAME)				do{ ; } while(false)
-	#define _NMSP_PROFILE_DYNAMIC_SCOPED()			do{ ; } while(false)
-	#define _NMSP_PROFILE_DYNAMIC_SECTION(NAME)		do{ ; } while(false)
+	#define NMSP_PROFILE_SECTION_IMPL(MSrcLocData, ...)				do{ ; } while(false)	
+
+	#define _NMSP_PROFILE_SCOPED()									do{ ; } while(false)
+	#define _NMSP_PROFILE_SECTION(NAME)								do{ ; } while(false)
+	#define _NMSP_PROFILE_DYNAMIC_SCOPED()							do{ ; } while(false)
+	#define _NMSP_PROFILE_DYNAMIC_SECTION(NAME)						do{ ; } while(false)
 	
-	#define _NMSP_PROFILE_TAG(STR)					do{ ; } while(false)
-	#define _NMSP_PROFILE_LOG(TEXT)					do{ ; } while(false)
-	#define _NMSP_PROFILE_VALUE(TEXT, VALUE)		do{ ; } while(false)
+	#define _NMSP_PROFILE_TAG(STR)									do{ ; } while(false)
+	#define _NMSP_PROFILE_LOG(TEXT)									do{ ; } while(false)
+	#define _NMSP_PROFILE_VALUE(TEXT, VALUE)						do{ ; } while(false)
 	
-	#define _NMSP_PROFILE_ALLOC(PTR, SIZE)			do{ ; } while(false)
-	#define _NMSP_PROFILE_FREE(PTR, SIZE)			do{ ; } while(false)
+	#define _NMSP_PROFILE_ALLOC(PTR, SIZE)							do{ ; } while(false)
+	#define _NMSP_PROFILE_FREE(PTR, SIZE)							do{ ; } while(false)
 	
-	#define _NMSP_PROFILE_SET_THREAD_NAME(NAME)		do{ ; } while(false)
+	#define _NMSP_PROFILE_SET_THREAD_NAME(NAME)						do{ ; } while(false)
 
 #endif // NMSP_ENABLE_INTERNAL_PROFILER
